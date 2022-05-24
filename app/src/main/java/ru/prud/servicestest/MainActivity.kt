9f94c1bf87637ef54999a68a29870c810f0a5123
Yury Prudnikov <y.prudnikov@growapp.biz>
2022-05-24 17:32:23 +0300

    add foreground service

--- app/src/main/java/ru/prud/servicestest/MainActivity.kt
+++ app/src/main/java/ru/prud/servicestest/MainActivity.kt
@@ -1,17 +1,13 @@
 package ru.prud.servicestest
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.NotificationCompat
-import ru.prud.servicestest.R
+import androidx.core.content.ContextCompat
 import ru.prud.servicestest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private var id = 0
+
 
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
@@ -24,31 +20,7 @@ class MainActivity : AppCompatActivity() {
             startService(MyService.newIntent(this, 25))
         }
         binding.foregroundService.setOnClickListener {
-            showNotification()
+           ContextCompat.startForegroundService(this, MyForegroundService.newIntent(this))
         }
-    }
-
-    private fun showNotification() {
-        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val notificationChannel = NotificationChannel(
-                CHANNEL_ID,
-                CHANNEL_NAME,
-                NotificationManager.IMPORTANCE_DEFAULT
-            )
-            notificationManager.createNotificationChannel(notificationChannel)
-        }
-        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setContentTitle("Title")
-            .setContentText("Text")
-            .setSmallIcon(R.drawable.ic_launcher_background)
-            .build()
-        notificationManager.notify(id++, notification)
-    }
-
-    companion object {
-
-        private const val CHANNEL_ID = "channel_id"
-        private const val CHANNEL_NAME = "channel_name"
     }
 }
